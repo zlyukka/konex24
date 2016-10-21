8a6d5d0b5fe7ca3b09680ac2671b7ee477a37a10
zlyukka <vitaliy_kov@mail.ru>
2016-10-21 17:18:59 +0300

    Добавлен класс карт

---
+++ src/main/resources/static/js/script.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Ruslan on 05.10.2016.
+ */
+function alertWork() {
+    //alert("Work.");
+    console.log("Work!")
+}
---
+++ src/test/java/com/konex/Konex24ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.konex;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Konex24ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
